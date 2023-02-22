d6b4e64fe5b5012326165ce37443357ab1d83542
Mumbi njuguna <mumbijoynjuguna@gmail.com>
2023-02-22 12:39:43 +0300

    reg form in android

---
+++ app/src/main/java/com/example/formlinked/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.formlinked;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.content.Intent;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    public Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button = (Button) findViewById(R.id.btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), activity2.class);
+                //Intent intent = new Intent (MainActivity.this,activity2.class)
+                startActivity(intent);
+            }
+
+
+        });
+    }
+}
